ca7537724dd344e65dfc80895f85f36468f455f9
Leonardo Seguel <leonardo.seguel03@inacapmail.cl>
2023-10-25 16:50:09 -0300

    Arreglado para Android API 34, creo

--- app/src/main/java/com/example/englishapp/MainActivity.kt
+++ app/src/main/java/com/example/englishapp/MainActivity.kt
@@ -1,88 +1,69 @@
 package com.example.englishapp
 
-import android.app.Activity
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import java.io.BufferedReader
-import java.io.InputStreamReader
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
-    private val READ_REQUEST_CODE: Int = 42
-    private val PERMISSION_REQUEST_STORAGE: Int = 1001
+    // Declara una variable TextView para mostrar texto desde un archivo
+    private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val openFileButton: Button = findViewById(R.id.openFileButton)
-        openFileButton.setOnClickListener {
-            // Check permission
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this,
-                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
-                    PERMISSION_REQUEST_STORAGE)
-            } else {
-                openFile()
+        // Inicializa la variable TextView
+        textView = findViewById(R.id.contentTextView)
+
+        // Busca el boton en el diseño y fija un listener de click
+        val button: Button = findViewById(R.id.openFileButton)
+        button.setOnClickListener {
+            // Crea un Intent para abrir el selector de archivos
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                // Se asegura de que el archivo se puede abrir
+                addCategory(Intent.CATEGORY_OPENABLE)
+                // Fija el tipo de archivo (.txt)
+                type = "text/plain"
             }
+            // Inicia la actividad del selector de archivos con un código de solicitud
+            startActivityForResult(intent, 1)
         }
     }
-
-    private fun openFile() {
-        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
-            addCategory(Intent.CATEGORY_OPENABLE)
-            type = "text/plain"
-        }
-        startActivityForResult(intent, READ_REQUEST_CODE)
-    }
-
+    // Esta función se activa cuando la actividad del selector de archivos devuelve un resultado
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+        // Comprueba si el código de solicitud coincide y el resultado es OK
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            // Obtiene la Uri del archivo seleccionado
             data?.data?.also { uri ->
-                readTextFromUri(uri)?.let {
-                    val contentTextView: TextView = findViewById(R.id.contentTextView)
-                    contentTextView.text = it
-                }
+                // Carga el texto del archivo seleccionado a través de su Uri
+                loadTextFromFile(uri)
             }
         }
     }
 
-    private fun readTextFromUri(uri: Uri): String? {
-        val stringBuilder = StringBuilder()
-        contentResolver.openInputStream(uri)?.use { inputStream ->
-            BufferedReader(InputStreamReader(inputStream)).use { reader ->
-                var line: String? = reader.readLine()
-                while (line != null) {
-                    stringBuilder.append(line)
-                    line = reader.readLine()
-                }
-            }
-        }
-        return stringBuilder.toString()
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        when (requestCode) {
-            PERMISSION_REQUEST_STORAGE -> {
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    openFile()
-                } else {
-                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
-                }
-                return
-            }
-            else -> {
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-            }
+    // Función para leer texto de un archivo especificado por una Uri
+    private fun loadTextFromFile(uri: Uri) {
+        try {
+            // Abre un InputStream para leer el archivo
+            val inputStream = contentResolver.openInputStream(uri)
+            // Crea un BufferedReader para leer texto desde el InputStream
+            val reader = BufferedReader(inputStream!!.reader())
+            // Lee tod0 el contenido y lo establece en el TextView
+            textView.text = reader.readText()
+            // Cierra el lector
+            reader.close()
+        } catch (e: IOException) {
+            // Maneja las excepciones
+            e.printStackTrace()
+            Toast.makeText(this, "Error reading file", Toast.LENGTH_SHORT).show()
         }
     }
 }
